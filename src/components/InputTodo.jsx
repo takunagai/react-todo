3bce7cd0b685030a1949a331aed36a0a5b666fe9
takna <tak_na@icloud.com>
2023-01-15 22:10:11 +0900

    入力フォームのスタイルを CSS in JS で実装

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,18 @@
 const InputTodo = ({todoText, setTodoText, onClickAdd}) => {
+
+    const style = {
+      padding: "8px",
+      width: "calc(100% - 5rem)",
+      outline: "none"
+    }
     return (
-      <div className="area input-area">
+      <div className="area">
         <input
           type="text"
           placeholder="TODO を入力"
           value={todoText}
           onChange={(e) => setTodoText(e.target.value)}
+          style={style}
         />
         <button onClick={onClickAdd}>追加</button>
       </div>
